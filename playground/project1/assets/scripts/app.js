6e099309ab313b341c502f82e43adc76f02eb124
k-nautresurrect <kushignite@gmail.com>
2022-07-17 22:45:52 +0530

    changed things

--- playground/project1/assets/scripts/app.js
+++ playground/project1/assets/scripts/app.js
@@ -4,7 +4,7 @@ import el from "./vendor.js";
 let result = 0;
 let resultStrArr = []; // array of objects
 let resultStr = "";
-let idx=0;
+// let idx=0;
 
 el.add.addEventListener("click", add);
 el.sub.addEventListener("click", subtract);
@@ -12,7 +12,6 @@ el.mul.addEventListener("click", multiply);
 el.div.addEventListener("click", divide);
 el.reset.addEventListener("click", reset);
 el.equal.addEventListener("click", equal);
-
 
 // take input from field
 function takeInput() {
@@ -53,25 +52,31 @@ function divide(e) {
   console.log(resultStrArr);
 }
 // reset field
-function reset(){
-  el.input.value=null;
-  result=0;
-  resultStr="";
-  showOutput(resultStr,result);
+function reset() {
+  el.input.value = null;
+  result = 0;
+  resultStr = "";
+  resultStrArr = [];
+  showOutput(resultStr, result);
 }
 //equal field
-function equal(){
-  
+function equal(e) {
+  let Str = prepareStr();
+  Str = Str.slice(0, Str.length - 2);
+  Str += `${e.target.textContent} ${result}`;
+  showOutput(Str, result);
 }
 
 // make a string
 function prepareStr() {
-    // resultStrArr.forEach((objVal, idx) => {
-    //   resultStr += `${objVal.num} ${objVal.sign} `;
-    // });
-    resultStr+=resultStrArr[idx].num+resultStrArr[idx].sign;
-    idx++;
-  showOutput(resultStr, result);
+  let str = "";
+  resultStrArr.forEach((objVal, idx) => {
+    str += `${objVal.num} ${objVal.sign} `;
+  });
+  // resultStr+=resultStrArr[idx].num+resultStrArr[idx].sign;
+  // idx++;
+  showOutput(str, result);
+  return str;
 }
 // show output
 function showOutput(resultStr, result) {
