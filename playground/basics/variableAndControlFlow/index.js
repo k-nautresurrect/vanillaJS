41085f7e77d6202c65ed17d70004e7406c333a06
k-nautresurrect <kushignite@gmail.com>
2022-07-20 19:32:38 +0530

    basic variable and control flow

---
+++ playground/basics/variableAndControlFlow/index.js
@@ -0,0 +1,20 @@
+// interpretor
+// on the fly compilation -> hand to hand compile and execute
+// repl -> read eval print loop
+// weakly typed language -> dnamically typed, variable proxy, literal
+var x; // defination
+// re - assignment
+// x -> 90, x -> string
+// vaiable proxy (x), lvalue (literal)
+console.log(x); // cout
+console.log(typeof x);
+
+// types
+// number (integer, float, double)
+// boolean (truthy list, falsy list){0 -> false, -ive or +ive true}
+// string (character, string)
+// objects (structs)
+
+// NaN not a number
+// null
+// undefined
